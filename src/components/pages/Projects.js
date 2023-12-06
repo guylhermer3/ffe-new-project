a3000b742dadcaac3185c6600ca4c8ee8e712828
guylhermer3 <guylherme_rodrygues@hotmail.com>
2023-12-06 16:04:22 -0400

    Implementei componente de loader

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 
 import Message from '../layout/Message'
 import Container from '../layout/Container'
+import Loading from '../layout/Loading'
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
 
@@ -11,6 +12,7 @@ import styles from './Projects.module.css'
 
 function Projects() {
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -19,18 +21,23 @@ function Projects() {
     }
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then((resp) => resp.json())
-        .then((data) => {
-            console.log(data)
-            setProjects(data)
-        })
-        .catch((err) => console.log(err))
+        setTimeout(
+            () => {
+
+                fetch('http://localhost:5000/projects', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log(data)
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))   
+            }, 300)
     }, [])
 
     return (
@@ -51,6 +58,10 @@ function Projects() {
                     key={project.id} 
                     />
                     ))}
+                    {!removeLoading && <Loading />}
+                    {removeLoading && projects.length === 0 && (
+                        <p>Não há projetos cadastrados!</p>
+                    )}
             </Container>
         </div>
     )
