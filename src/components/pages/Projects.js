0ed0f217dbe70b2e98108244953f43addb7c59d0
guylhermer3 <guylherme_rodrygues@hotmail.com>
2023-12-06 17:28:17 -0400

    Implementei excluir projeto

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -13,6 +13,7 @@ import styles from './Projects.module.css'
 function Projects() {
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = ''
@@ -21,9 +22,7 @@ function Projects() {
     }
 
     useEffect(() => {
-        setTimeout(
-            () => {
-
+        setTimeout(() => {
                 fetch('http://localhost:5000/projects', {
                     method: 'GET',
                     headers: {
@@ -40,6 +39,21 @@ function Projects() {
             }, 300)
     }, [])
 
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then((resp) => resp.json())
+        .then(() => {
+            setProjects(projects.filter((project) => project.id !== id))
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch(err => console.log(err))
+    }
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
@@ -47,6 +61,7 @@ function Projects() {
             <LinkButton to="/newproject" text="Criar Projeto" />
             </div>
             {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => (
@@ -55,7 +70,8 @@ function Projects() {
                     name={project.name}
                     budget={project.budget}
                     category={project.category.name}
-                    key={project.id} 
+                    key={project.id}
+                    handleRemove={removeProject} 
                     />
                     ))}
                     {!removeLoading && <Loading />}
